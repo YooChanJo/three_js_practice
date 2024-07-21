113debafae4b65b1e3f0eacfbd5dbd76d8d080a1
Yoo-Chan Jo <jo.yoochani@gmail.com>
2024-07-21 20:06:47 +0900

    Changed another import

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from "./node_modules/three/build/three.module.js"
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);

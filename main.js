1cf7a88f98b09b85982da05cad960cce60911f47
Yoo-Chan Jo <jo.yoochani@gmail.com>
2024-07-21 20:49:27 +0900

    Changed dependency

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as THREE from "./three/build/three.module.js"
 import { OrbitControls } from './three/examples/jsm/controls/OrbitControls.js';
 
-// false comment
+// All three libs require path change for github usage, please consider it before deploy
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
